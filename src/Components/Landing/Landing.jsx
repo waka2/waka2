dd547bdc3a9eb4c8556af95d7aee1076e5ceae37
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-29 11:10:00 -0600

    added nested components

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import ClassicGame from './ClassicGame/ClassicGame'
 
 function Landing() {
     return(
         <div className="landing">
+            <ClassicGame />
         </div>
     )
 }

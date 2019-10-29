c91e0fe4f3621c1d30c52eb5586247421abbe126
McKay Waters <mckaypaswaters@gmail.com>
2019-10-29 11:21:14 -0600

    working on custom fonts

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import './landing.scss'
 
 function Landing() {
     return(
         <div className="landing">
+            <h1 className='landing-test'>Waka<sup className='number'>2</sup></h1>
         </div>
     )
 }

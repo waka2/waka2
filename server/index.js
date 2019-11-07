81f9c36f1db7ceea99d730ff1774a2f547f1faa5
McKay Waters <mckaypaswaters@gmail.com>
2019-11-07 16:44:28 -0700

    im so sick of this

--- server/index.js
+++ server/index.js
@@ -13,16 +13,16 @@ const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
 
 const app = express();
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
+app.use(express.static(`${__dirname}/../build`))
 app.use(express.json())
 app.use(session({
     resave: false,
     saveUninitialized: true,
     secret: SESSION_SECRET
 }))
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 
 // Get Score - /api/score

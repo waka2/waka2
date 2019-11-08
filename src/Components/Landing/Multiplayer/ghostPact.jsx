390ad418c1090fc9f49730e1287682e4e103097d
McKay Waters <mckaypaswaters@gmail.com>
2019-11-08 08:59:44 -0700

    added win logic on two player

--- src/Components/Landing/Multiplayer/ghostPact.jsx
+++ src/Components/Landing/Multiplayer/ghostPact.jsx
@@ -3,6 +3,7 @@ import MPBoard from './Board/MPBoard.jsx'
 import Header from '../ClassicGame/Header/Header'
 import HeaderMP from './HeaderMP/HeaderMP'
 // import Footer from '../ClassicGame/Footer/Footer'
+import './ghostpact.scss'
 
 class Multiplayer extends Component {
     constructor(){
@@ -10,7 +11,7 @@ class Multiplayer extends Component {
         this.state = {
             pacmanOnePoints: 0,
             pacmanTwoPoints: 0,
-            hiddenPoints: 2300
+            hiddenPoints: 0
         }
     }
     addPoints = (num, id) => {
@@ -28,12 +29,52 @@ class Multiplayer extends Component {
             hiddenPoints: this.state.hiddenPoints + num
         })
     }
+    winCondition = () => {
+        if (this.state.pacmanOnePoints > this.state.pacmanTwoPoints){
+            return (
+                <div className="winScreens">
+                    <div className="emptyHeader"></div>
+                    <div className="playerOneWinsScreen">
+                        <h1 className='blink-win'>Player One Wins!</h1>
+                        <button onClick={() => this.refreshPage()} className='push--skeuo'></button>
+                        <h3 className='blink-win'>Play Again?</h3>
+                    </div>
+                </div>
+            )
+        } else if (this.state.pacmanTwoPoints > this.state.pacmanOnePoints){
+            return (
+                <div className="winScreens">
+                    <div className="emptyHeader"></div>
+                    <div className="playerTwoWinsScreen">
+                        <h1 className='blink-win'>Player Two Wins!</h1>
+                        <button onClick={() => this.refreshPage()} className='push--skeuo'></button>
+                        <h3 className='blink-win'>Play Again?</h3>
+                    </div>
+                </div>
+            )
+        } else if (this.state.pacmanOnePoints === this.state.pacmanTwoPoints){
+            return (
+                <div className="winScreens">
+                    <div className="emptyHeader"></div>
+                    <div className="drawScreen">
+                        <h1 className='blink-win'>It's a Draw!</h1>
+                        <button onClick={() => this.refreshPage()} className='push--skeuo'></button>
+                        <h3 className='blink-win'>Play Again?</h3>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    refreshPage(){
+        window.location.reload()
+    }
 
     render(){
         return(
             <div id="multiplayer">
-                {this.state.hiddenPoints >= 2400 ? 
-                alert('You Win!') :
+                {this.state.hiddenPoints >= 2600 ? 
+                this.winCondition() :
                 <div>
                 <HeaderMP hiddenPoints={this.state.hiddenPoints} 
                 pacmanOnePoints={this.state.pacmanOnePoints} 
